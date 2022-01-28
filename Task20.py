5f88a7418525251c83e33be565cc69f07178a434
AlenaKovganko <92676854+AlenaKovganko@users.noreply.github.com>
2022-01-29 00:01:27 +0300

    20.1

--- Task20.py
+++ Task20.py
@@ -1,8 +1,8 @@
 # 20. Задать номер четверти, показать диапазоны для возможных координат
 def Number(a):
-    if a == 2: print('x > 0 and y > 0')
+    if a == 1: print('x > 0 and y > 0')
     elif a == 2: print('x < 0 and y > 0')
     elif a == 3: print('x < 0 and y < 0')
     elif a == 4: print ('x > 0 and y < 0')
     else: print('Номер некорректен')
-Number(a = int(input('Введите номер четверти')))
+Number(a = int(input('Введите номер четверти  ')))
