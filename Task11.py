77da427f4da2a2308304b41c78db5ea863205666
AlenaKovganko <92676854+AlenaKovganko@users.noreply.github.com>
2022-01-28 23:10:01 +0300

    11

--- Task11.py
+++ Task11.py
@@ -1,7 +1,7 @@
 # 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 def MaxNumber (a):
-    if a/10 > a%10: print (a/10)
-    elif a/10 < a%10: print (a%10)
+    if round(a/10) > round(a%10): print (a/10)
+    elif round(a/10) < round(a%10): print (a%10)
     else: print ('Цифры равны')
 
-MaxNumber (a = int(input('Введите число от 10 до 100')))
+MaxNumber (a = int(input('Введите число от 10 до 100  ')))
