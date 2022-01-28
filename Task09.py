6f5a70463631617fa4cac4d6ef7136248c895b85
AlenaKovganko <92676854+AlenaKovganko@users.noreply.github.com>
2022-01-28 22:43:50 +0300

    10

---
+++ Task09.py
@@ -0,0 +1,4 @@
+# 9.Показать последнюю цифру трёхзначного числа
+def printThirdNumber(a):
+    print (int(a %10))
+printThirdNumber (a  = int(input('Введите число ')))
---
+++ Task10.py
@@ -0,0 +1,4 @@
+# Показать вторую цифру трёхзначного числа
+def printSecondNumber(a):
+    print (int(a /10 % 10))
+printSecondNumber(a  = int(input('Введите число ')))
