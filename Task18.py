1f8576701aa7f2e6f67ddf09155874f75f661aac
AlenaKovganko <92676854+AlenaKovganko@users.noreply.github.com>
2022-01-29 16:36:52 +0300

    18

--- Task18.py
+++ Task18.py
@@ -1,5 +1,5 @@
 # Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
 def truth_false(x, y):
-    if not (x or y) == (not x) and (not y): print(True)
+    if not (x or y) == (not x and not y): print(True)
     else: print (False)
 truth_false(x = int(input('Enter x:')), y = int(input('Enter y:')))
