7bd0e69156c42fa2bbc48973f4bd8038648a8879
AlenaKovganko <92676854+AlenaKovganko@users.noreply.github.com>
2022-02-02 16:10:34 +0300

    46

---
+++ Task46.py
@@ -0,0 +1,14 @@
+# 46 Дано число. Составить список чисел Фибоначчи, в том числе для отрицательных индексов
+def fib(n):
+    if n in [1,2]: return 1
+    if n > 0: return fib(n-1) + fib (n-2)
+    if n < 0: return (fib(n+2) - fib (n+1))
+        
+def fib2(N1, N2):
+    fibon = []
+    for i in range (N1, N2):
+        fibon.append(fib(i))
+    return fibon
+
+print(fib2(N1 = int(input('Введите начальное число')),N2 = int(input('Введите начальное число'))))
+
