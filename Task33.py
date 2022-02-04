0217e17236695e238580b032dd9bf1ad7f12a428
AlenaKovganko <92676854+AlenaKovganko@users.noreply.github.com>
2022-02-04 21:06:23 +0300

    33

--- Task33.py
+++ Task33.py
@@ -1,13 +1,13 @@
 # 33 Пользователь задаёт две строки. Определить количество количество вхождений одной строки в другой.
 def string(str1, str2):
     count = 0
-    symbol = ''
-    u = 0
-    while u < len(str2):
+    s = ''
+    a = 0
+    while a < len(str2):
         for i in str1:
             for j in str2:
                 if i==j:
                     count+=1
-        u+=1
-    return(count // u) // u
+        a+=1
+    return(count // a) // a
 print(string(str1 = input('Введите строку 1 '),str2 = input('Введите строку 2 ')))
