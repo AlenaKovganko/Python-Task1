3cd07a8801e8ca48270ff903a54dee12a934bb88
AlenaKovganko <92676854+AlenaKovganko@users.noreply.github.com>
2022-02-05 12:03:15 +0300

    41

---
+++ Task41.py
@@ -0,0 +1,13 @@
+# 41 Определить, позицию второго вхождения строки в списке либо сообщить, что его нет.
+list = ['a', 'b', 'c', 'd', 'a']
+def find(text):
+    res = 0
+    for i in range(0, len(list)):
+        if text == list[i]:
+            res += 1
+            if res == 2:
+                print(i)
+    if res<2:
+        print('Второго вхождения нет')
+find(text = (input('Введите  ')))
+
